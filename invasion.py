28a298c7df9bf2313f02ad29fd493ea5f2ccf9d7
IgnacioRisso <gamerdream406@gmail.com>
2022-11-13 15:01:10 -0300

    Update invasion.py
    Documentación

--- invasion.py
+++ invasion.py
@@ -2,16 +2,22 @@ import sys
 import pygame
 
 def run_game():
+    # Inicializar el juego y crear un objeto llamado pantalla.
     pygame.init()
     screen = pygame.display.set_mode((800, 600))
     pygame.display.set_caption("Space Invaders")
 
+    bg_color = (230, 230, 230)
+
+    # Iniciar el bucle principal del juego.
     while True:
 
+        # Responder a la entrada del teclado o del ratón.
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 sys.exit()
 
+        # Hacer visible la pantalla dibujada más reciente.
         pygame.display.flip()
 
 run_game()        
